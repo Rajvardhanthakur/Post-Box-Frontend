4ce575895cd5d1023c7fce6c6ab96bc7dbf71bb5
Rajvardhan <rajthakur3619@gmail.com>
2020-06-16 18:42:50 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Provider } from 'react-redux';
+
+import {store} from './actions/store';
+import PostMessages from './components/PostMessages';
+import { Container, AppBar, Typography } from "@material-ui/core";
+import ButterToast, { POS_CENTER, POS_BOTTOM} from 'butter-toast';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <Provider store={store}>
+      <Container maxWidth="lg">
+        <AppBar position="static" color="inherit">
+          <Typography variant="h2" align="center">
+            Post Box
+          </Typography>
+        </AppBar>
+      <PostMessages />
+      <ButterToast position={{vertical:POS_BOTTOM, horizontal: POS_CENTER}} />
+      </Container>
+    </Provider>
   );
 }
 
